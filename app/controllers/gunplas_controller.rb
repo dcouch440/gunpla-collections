067b68b70b50270f6e5c5f23cebe8445958bc379
Jon Stump <jmstump@gmail.com>
2021-04-05 11:59:09 -0700

    add gunpla controller
    Co-authored-by: David Couch <dcouch440@gmail.com>
    Co-authored-by: Kerry Lang <klang812@gmail.com>

---
+++ app/controllers/gunplas_controller.rb
@@ -0,0 +1,53 @@
+class GunplaController < ApplicationController
+  def index
+    @gunpla = Gunpla.all
+    render :index
+  end
+
+  def show
+    @gunpla = Gunpla.find(params[:id])
+    render :show
+  end
+
+  def create
+    @gunpla = Gunpla.new(gunpla_params)
+    if @gunpla.save
+      flash[:notice] = 'Gunpla successfully created'
+      redirect_to gunplas_path
+    else
+      render :new
+    end
+  end
+
+  def new
+    @gunpla = Gunpla.new
+    render :new
+  end
+
+  def edit
+    @gunpla = Gunpla.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @gunpla = Gunpla.find(params[:id])
+    if @gunpla.update(gunpla_params)
+      flash[:notice] = 'Gunpla successfully updated'
+      redirect_to gunplas_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @gunpla = Gunpla.find(params[:id]).destroy
+    flash[:alert] = 'Gunpla has been successfully deleted'
+    redirect to gunplas_path
+  end
+
+  private
+
+  def gunpla_params do
+    params.require(:gunpla).permit(:kit_name, :gundam_name, :gundam_series, :grade, :scale)
+  end
+end
