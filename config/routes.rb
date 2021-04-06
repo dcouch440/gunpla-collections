47ce5f3543b7d3022187dd1e9332bc89800a6523
Jon Stump <jmstump@gmail.com>
2021-04-05 17:02:14 -0700

    update routes
    Co-authored-by: Kerry Lang <klang812@gmail.com>
    Co-authored-by: David Couch <dcouch440@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,11 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'gunplas#index'
 
-  post 'sign-in', to: 'sessions#sign_in'
-  delete 'sign-out', to: 'sessions#sign_out'
+  # post 'sign-in', to: 'sessions#sign_in'
+  # delete 'sign-out', to: 'sessions#sign_out'
 
-  resources :users, except: [:index] do
-    resources :gunplas, only: [:index, :show]
-  end
   resources :gunplas, only: [:index, :show] do
     resources :reviews, except: [:index]
   end
+
 end
