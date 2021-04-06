6d8587f3d3d0e7b691fe665eee4472c766a8293b
Jon Stump <jmstump@gmail.com>
2021-04-06 09:26:37 -0700

    add active storage and update rails admin
    Co-authored-by: David Couch <dcouch440@gmail.com>
    Co-authored-by: Kerry Lang <klang812@gmail.com>

--- app/models/gunpla.rb
+++ app/models/gunpla.rb
@@ -1,6 +1,7 @@
 class Gunpla < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_and_belongs_to_many :users
+  has_many_attached :images, dependent: :destroy
 
   validates *%i[kit_name gundam_name grade scale gundam_series],
     presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   has_and_belongs_to_many :gunplas
   validates *%i[username email password],
   presence: true
+  has_one_attached :avatar
 
 end
