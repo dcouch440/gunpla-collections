3f00709e9e7a77c065a6f4eeed5f47aa1b378bfb
Jon Stump <jmstump@gmail.com>
2021-04-07 09:27:59 -0700

    update seeds to better utilize account avatars
    Co-authored-by: David Couch <dcouch440@gmail.com>
    Co-authored-by: Kerry Lang <klang812@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,71 @@ Review.destroy_all
 end
 
 p "Created #{User.count} users and #{Review.count} reviews."
+
+
+
+
+
+
+class Seed
+
+  def self.run_seed
+    User.destroy_all
+    seed = Seed.new
+    15.times {User.create(user_hash)}
+    seed.attach_avatars_to_users
+  end
+
+
+  def self.gen_paragraph
+    paragraph = []
+    3.times do
+      paragraph << ('<p>' + Faker::Lorem.sentence(word_count: 14, supplemental: false, random_words_to_add: 14) + '</p>')
+    end
+    paragraph.join('')
+  end
+
+  def self.user_hash
+    {
+      email: 'u@u.com',
+      password: 'foo',
+      content_body: gen_paragraph()
+    }
+  end
+
+  def get_dir_length
+    Dir["#{::Rails.root}/storage/defaults/*.*"].length
+  end
+
+  def get_dir_array
+    Dir["#{::Rails.root}/storage/defaults/*.*"]
+  end
+
+  def get_dir_hash_using_index(index)
+    dir = get_dir_array()[index]
+    {
+      io: File.open(dir),
+      filename: dir.split('/')[-1],
+      content_type: "image/#{dir.split('.')[-1]}"
+    }
+  end
+
+  def attach_avatars_to_users
+
+    current_index = -1
+    User.all.each do |user|
+
+      if current_index >= get_dir_length() - 1
+        current_index = 0
+        user.avatar.attach(get_dir_hash_using_index(current_index))
+      else
+        current_index += 1
+        user.avatar.attach(get_dir_hash_using_index(current_index))
+      end
+
+    end
+  end
+
+end
+
+Seed.run_seed()
