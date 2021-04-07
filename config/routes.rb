cc94400f04efcfc2cdf0f962bbcfc2ce41f0ed83
Jon Stump <jmstump@gmail.com>
2021-04-07 11:49:20 -0700

    add routes for user collection
    Co-authored-by: David Couch <dcouch440@gmail.com>
    Co-authored-by: Kerry Lang <klang812@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
+
   devise_for :users
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   root to: 'gunplas#index'
+
+  get '/users/:id/collection', to: 'gunplas#collection'
 
   # post 'sign-in', to: 'sessions#sign_in'
   # delete 'sign-out', to: 'sessions#sign_out'
 
   resources :gunplas, only: [:index, :show] do
+    post 'collection', to: 'gunplas#add_collection'
     resources :reviews, except: [:index]
   end
 
